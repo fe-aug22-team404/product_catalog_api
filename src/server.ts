848478e2b0b27b52b7e5a4b4a9453b5e4ba25658
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-12-01 11:29:44 +0200

    pg fixes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import serverless from 'serverless-http';
+// import serverless from 'serverless-http';
 import { phonesController } from './controllers/phone';
 import { PhoneData } from './data/models/phones';
 import { phonesDescriptionController } from './controllers/phoneDescription';
 import { PhoneDescription } from './data/models/phoneDescription';
 
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 const phonesRouter = express.Router();
 const phoneDescriptionRouter = express.Router();
@@ -25,9 +25,9 @@ phoneDescriptionRouter.post('/', phonesDescriptionController.postDescription);
 PhoneData.sync();
 PhoneDescription.sync();
 
-// app.listen(port, () => {
-//   console.log('server started ', port);
-// });
-app.use('/.netlify/functions/server', phonesRouter);
+app.listen(port, () => {
+  console.log('server started ', port);
+});
+// app.use('/.netlify/functions/server', phonesRouter);
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
